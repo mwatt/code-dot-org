0b8387bf0db2e8e2b70b6943dad4f38518bd7b31
David Wu <davidcnwu@gmail.com>
2015-10-09 18:21:28 -0700

    spec change.

--- dashboard/app/controllers/transfers_controller.rb
+++ dashboard/app/controllers/transfers_controller.rb
@@ -5,21 +5,52 @@ class TransfersController < ApplicationController
   def create
     new_section_code = params[:id]
 
-    if !params.has_key?(:student_ids)
+    if params.has_key?(:current_section_code)
+      current_section_code = params[:current_section_code]
+    else
       # TODO: i18n
       render json: {
-        error: "Student IDs not provided"
+        error: "Please provide current_section_code."
       }, status: :bad_request
       return
     end
 
-    student_ids = params[:student_ids].split(',').map(&:to_i)
+    if params.has_key?(:stay_enrolled_in_current_section)
+      stay_enrolled_in_current_section = params[:stay_enrolled_in_current_section]
+    else
+      # TODO: i18n
+      render json: {
+        error: "Please provide stay_enrolled_in_current_section."
+      }, status: :bad_request
+      return
+    end
+
+    if params.has_key?(:student_ids)
+      student_ids = params[:student_ids].split(',').map(&:to_i)
+    else
+      # TODO: i18n
+      render json: {
+        error: "Please provide student_ids."
+      }, status: :bad_request
+      return
+    end
 
     begin
-      section = Section.find_by!(code: new_section_code)
+      current_section = Section.find_by!(code: current_section_code)
     rescue ActiveRecord::RecordNotFound
+      # TODO: i18n
       render json: {
-        error: "Section does not exist"
+        error: "Sorry, but section #{current_section_code} does not exist. Please enter a different section code."
+      }, status: :not_found
+      return
+    end
+
+    begin
+      new_section = Section.find_by!(code: new_section_code)
+    rescue ActiveRecord::RecordNotFound
+      # TODO: i18n
+      render json: {
+        error: "Sorry, but that section does not exist. Please enter a different section code."
       }, status: :not_found
       return
     end
@@ -29,30 +60,47 @@ class TransfersController < ApplicationController
     rescue ActiveRecord::RecordNotFound
       # TODO: i18n
       render json: {
-        error: "One or more students could not be found"
+        error: "One or more students could not be found."
       }, status: :not_found
       return
     end
 
     if student_ids.count != Follower.where(student_user_id: student_ids).count
+      # TODO: i18n
       render json: {
-        error: "All the students must currently be enrolled in your section(s)"
+        error: "All the students must currently be enrolled in your section."
       }, status: :forbidden
       return
     end
 
-    # mass update students
+
     students.each do |student|
-      follower_same_user_teacher = student.followeds.find_by_user_id(section.user_id)
-      if follower_same_user_teacher.present?
-        follower_same_user_teacher.update_attributes!(section_id: section.id)
-      else
-        Follower.create!(user_id: section.user_id, student_user: student, section: section)
+      if !student.followeds.exists?(section_id: new_section.id)
+        student.followeds.create!(user_id: new_section.user_id, section: new_section)
+      end
+
+      if !stay_enrolled_in_current_section
+        student.followeds.find_by_section_id(current_section.id).destroy
       end
 
       # assign_script for each student
-      student.assign_script(section.script) if section.script
+      student.assign_script(new_section.script) if new_section.script
     end
+
+    # mass update students
+    # students.each do |student|
+    #   follower_same_user_teacher = student.followeds.find_by_user_id(new_section.user_id)
+    #   if follower_same_user_teacher.present?
+    #     follower_same_user_teacher.update_attributes!(section_id: new_section.id)
+    #   else
+    #     Follower.create!(user_id: new_section.user_id, student_user: student, section: new_section)
+    #   end
+    #
+    #   # assign_script for each student
+    #   student.assign_script(new_section.script) if new_section.script
+    # end
+
+    # TODO: Email students if they're transferred to another teacher
 
     render json: {}, status: :no_content
   end
--- dashboard/test/controllers/transfers_controller_test.rb
+++ dashboard/test/controllers/transfers_controller_test.rb
@@ -15,14 +15,36 @@ class TransfersControllerTest < ActionController::TestCase
   end
 
   test "returns an error when student ids are not provided" do
-    post :create, id: @picture_section.code
+    post :create, id: @picture_section.code, current_section_code: @word_section.code, stay_enrolled_in_current_section: false
     assert_response 400
+    assert_equal "Please provide student_ids.", json_response["error"]
   end
 
   test "returns an error when the section id is invalid" do
     code = "QWERTY"
-    post :create, id: code, student_ids: @word_user_1.id.to_s
+    post :create, id: code, student_ids: @word_user_1.id.to_s, current_section_code: @word_section.code, stay_enrolled_in_current_section: false
     assert_response 404
+    assert_equal "Sorry, but that section does not exist. Please enter a different section code.", json_response["error"]
+  end
+
+  test "returns an error when the current_section_code is not provided" do
+    post :create, id: @picture_section.code, student_ids: @word_user_1.id.to_s, stay_enrolled_in_current_section: false
+    assert_response 400
+    assert_equal "Please provide current_section_code.", json_response["error"]
+  end
+
+  test "returns an error when stay_enrolled_in_current_section is not provided" do
+    post :create, id: @picture_section.code, student_ids: @word_user_1.id.to_s, current_section_code: @word_section.code
+    assert_response 400
+    assert_equal "Please provide stay_enrolled_in_current_section.", json_response["error"]
+  end
+
+  test "returns an error when the current_section_code is invalid" do
+    invalid_section_code = "QWERTY"
+
+    post :create, id: @picture_section.code, student_ids: @word_user_1.id.to_s, current_section_code: invalid_section_code, stay_enrolled_in_current_section: false
+    assert_response 404
+    assert_equal "Sorry, but section #{invalid_section_code} does not exist. Please enter a different section code.", json_response["error"]
   end
 
   test "returns an error when one of the student_ids is invalid" do
@@ -38,6 +60,16 @@ class TransfersControllerTest < ActionController::TestCase
     # Ergh, Devise will return a 302 instead of a 401 because of the way
     # this route is set up
     assert_response 302
+  end
+
+  test "students should stay enrolled in the current section if stay_enrolled_in_current_section is true" do
+    post :create, id: @picture_section.code, student_ids: @word_user_1.id.to_s, current_section_code: @word_section.code, stay_enrolled_in_current_section: true
+    assert Follower.exists?(student_user: @word_user_1, section: @word_section)
+  end
+
+  test "students should no longer be in the current section if stay_enrolled_in_current_section is false" do
+    post :create, id: @picture_section.code, student_ids: @word_user_1.id.to_s, current_section_code: @word_section.code, stay_enrolled_in_current_section: false
+    assert_not Follower.exists?(student_user: @word_user_1, section: @word_section)
   end
 
   test "you should only be able to transfer students who are currently in your section" do
