e443805554d62d4ba99311771fc827c5abbe73a3
Josh Lory <josh.lory@code.org>
2015-11-18 15:10:05 -0800

    Add unit test, fix jshint

--- shared/js/initApp.js
+++ shared/js/initApp.js
@@ -55,7 +55,7 @@ window.apps = {
           // timestamp initially (it will be updated with a timestamp from the server
           // if we get a response.
           lastSavedProgram = report.program;
-          dashboard.clientState.writeSourceForLevel(appOptions.level.scriptLevelId, +new Date, lastSavedProgram);
+          dashboard.clientState.writeSourceForLevel(appOptions.level.scriptLevelId, +new Date(), lastSavedProgram);
         }
         report.scriptName = appOptions.scriptName;
         report.fallbackResponse = appOptions.report.fallback_response;
