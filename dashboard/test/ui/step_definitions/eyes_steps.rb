1bee55aff61bc00b73701bee5424a9e2fb5615cd
David Bailey <davidsbailey@users.noreply.github.com>
2015-12-16 07:23:01 -0800

    make eyes tests always 1024x698

--- dashboard/test/ui/step_definitions/eyes_steps.rb
+++ dashboard/test/ui/step_definitions/eyes_steps.rb
@@ -3,7 +3,8 @@ require 'eyes_selenium'
 When(/^I open my eyes to test "([^"]*)"$/) do |test_name|
   ensure_eyes_available
   @original_browser = @browser
-  @browser = @eyes.open(app_name: 'Code.org', test_name: test_name, driver: @browser)
+  viewport_size = Struct.new(:width, :height).new(1024, 698)
+  @browser = @eyes.open(app_name: 'Code.org', test_name: test_name, driver: @browser, viewport_size: viewport_size)
 end
 
 And(/^I close my eyes$/) do
