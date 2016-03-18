86e0170c7f8a95e19424f26bc8b44c1949118ab9
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-18 16:35:37 -0700

    move dashboard to code-studio

--- code-studio/src/js/code-studio.js
+++ code-studio/src/js/code-studio.js
@@ -39,6 +39,7 @@ window.dashboard.popupWindow = require('./popup-window');
 window.dashboard.progress = require('./progress');
 window.dashboard.reporting = require('./reporting');
 window.dashboard.utils = require('./utils');
+window.dashboard.header = require('./header');
 
 // Wrap existing window onerror caller with a script error check.  If we have a
 // script error and a url, throw that so that we have the info in new relic.
