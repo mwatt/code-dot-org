f3eef0dd44a9eb245286166620f70f53f788b7c1
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-31 12:08:50 -0700

    generate style constants

---
+++ apps/src/styleConstants.js
@@ -0,0 +1,8 @@
+// color.js
+// GENERATED FILE: DO NOT MODIFY DIRECTLY
+// This generated file exports all colors defined in /Users/brent/git/cdo/shared/css/style-constants.scss
+// for use in JavaScript. The generator script is convert-scss-variables.js
+module.exports = {
+  workspace_headers_height: "30",
+  resize_bar_width: "13",
+};
