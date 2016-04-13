6b09e471971c2e7bf01c0b93bb4ca28008475f69
Mehal Shah <mehal@code.org>
2016-04-13 11:30:15 -0700

    More stuff

--- dashboard/app/helpers/plc/learning_modules_helper.rb
+++ dashboard/app/helpers/plc/learning_modules_helper.rb
@@ -2,4 +2,10 @@ module Plc::LearningModulesHelper
   def options_for_plc_task_learning_modules
     Plc::LearningModule.all.pluck(:name, :id).sort
   end
+
+  def options_for_evaluation_answer_modules(course_unit)
+    course_unit.plc_learning_modules.order(:required, :name).map do |course_unit|
+      [course_unit.name += (course_unit.required ? ' - Required' : ''), course_unit.id]
+    end
+  end
 end
