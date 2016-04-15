f9a03ac573228a2887173a723c86011e396e97ff
Asher Kach <asher@code.org>
2016-04-15 11:52:12 -0500

    Update section_api_helpers to properly account for deleted users.

--- pegasus/helpers/section_api_helpers.rb
+++ pegasus/helpers/section_api_helpers.rb
@@ -9,8 +9,10 @@ class DashboardStudent
   def self.fetch_user_students(user_id)
     Dashboard.db[:users].
       join(:followers, :student_user_id=>:users__id).
+      join(Sequel.as(:users, :users_students), :id=>:followers__student_user_id).
       select(*fields).
       where(followers__user_id: user_id).
+      where(users_students__deleted_at: nil).
       all
   end
 
@@ -50,22 +52,28 @@ class DashboardStudent
     return unless user && (user.followed_by?(id) || user.admin?)
 
     row = Dashboard.db[:users].
+      where(users__id: id).
+      where(users__deleted_at: nil).
       left_outer_join(:secret_pictures, id: :secret_picture_id).
       select(*fields,
              :secret_pictures__name___secret_picture_name,
              :secret_pictures__path___secret_picture_path,
             ).
-      where(users__id: id).
       server(:default).
       first
+
+    if row.nil?
+      return
+    end
 
     row.merge(age: birthday_to_age(row[:birthday]))
   end
 
   def self.update_if_allowed(params, dashboard_user_id)
+    return unless Dashboard.db[:users].
+      where(user_id: params[:id]).where(deleted_at: nil).first
     return unless Dashboard.db[:followers].
-      where(student_user_id: params[:id],
-            user_id: dashboard_user_id)
+      where(student_user_id: params[:id], user_id: dashboard_user_id).first
 
     fields = {updated_at: DateTime.now}
     fields[:name] = params[:name] unless params[:name].nil_or_empty?
@@ -256,7 +264,7 @@ class DashboardSection
 
   def self.fetch_if_allowed(id, user_id)
     # TODO: Allow caller to specify fields that they want because the
-    # recursion is getting a bit out of control (eg. you don't want to
+    # joins are getting a bit out of control (eg. you don't want to
     # get all the students passwords when we get the list of sections).
 
     return nil unless row = Dashboard.db[:sections].
--- pegasus/test/test_v2_user_routes.rb
+++ pegasus/test/test_v2_user_routes.rb
@@ -99,6 +99,14 @@ class V2UserRoutesTest < Minitest::Test
         assert_equal([expected_v2_students_hash_for(FakeDashboard::STUDENT)],
                      JSON.parse(@pegasus.last_response.body))
       end
+
+      it 'returns no deleted students' do
+        with_role FakeDashboard::TEACHER_WITH_DELETED_STUDENTS
+        @pegasus.get '/v2/students'
+        assert_equal 200, @pegasus.last_response.status
+        assert_equal([expected_v2_students_hash_for(FakeDashboard::STUDENT)],
+                     JSON.parse(@pegasus.last_response.body))
+      end
     end
 
     V2_STUDENTS_ID_KEY_LIST = V2_STUDENTS_KEY_LIST.dup.concat [:secret_picture_name,
@@ -139,6 +147,38 @@ class V2UserRoutesTest < Minitest::Test
         assert_equal 200, @pegasus.last_response.status
         assert_equal(expected_v2_students_id_hash_for(FakeDashboard::STUDENT),
                      JSON.parse(@pegasus.last_response.body))
+      end
+
+      it 'returns 403 when seeking nonexistent student' do
+        with_role FakeDashboard::ADMIN
+        @pegasus.get "/v2/students/#{FakeDashboard::UNUSED_USER_ID}"
+        assert_equal 403, @pegasus.last_response.status
+      end
+
+      it 'returns 403 when seeking deleted student' do
+        with_role FakeDashboard::ADMIN
+        @pegasus.get "/v2/students/#{FakeDashboard::DELETED_STUDENT[:id]}"
+        assert_equal 403, @pegasus.last_response.status
+      end
+    end
+
+    describe 'POST /v2/students/:id/update' do
+      it 'returns 403 "Forbidden" when signed in as another student' do
+        with_role FakeDashboard::STUDENT
+        @pegasus.post "/v2/students/#{FakeDashboard::SELF_STUDENT[:id]}/post"
+        assert_equal 403, @pegasus.last_response.status
+      end
+
+      it 'returns 403 "Forbidden" when signed in as unconnected teacher' do
+        with_role FakeDashboard::TEACHER
+        @pegasus.post "/v2/students/#{FakeDashboard::SELF_STUDENT[:id]}/post"
+        assert_equal 403, @pegasus.last_response.status
+      end
+
+      it 'updates student info when signed in as the teacher' do
+        with_role FakeDashboard::TEACHER
+        @pegasus.post "/v2/students/#{FakeDashboard::STUDENT[:id]}/post"
+        assert_equal 200, @pegasus.last_response.status
       end
     end
 
