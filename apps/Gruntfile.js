a7bc418963cc02a80c4150ee14686c0bdf403d2b
Paul Carduner <paul@carduner.net>
2016-05-03 17:47:25 -0700

    Log build times and add script to summarize them

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -2,9 +2,31 @@ var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var glob = require('glob');
+var execSync = require('child_process').execSync;
+var write = process.stdout.write.bind(process.stdout);
+
+function log(str) {
+	write(str + '\n', 'utf8');
+}
 
 module.exports = function (grunt) {
-  require('time-grunt')(grunt);
+  var email = 'unknown';
+  try {
+    email = execSync('git config --get user.email').toString().trim();
+  } catch (e) {
+    // I guess we are not in a git checkout, or don't have git installed.
+  }
+  require('time-grunt-nowatch')(grunt, false, function (stats, done) {
+    try {
+      fs.appendFileSync(
+        path.join(__dirname, 'build-times.log'),
+        JSON.stringify([new Date().toString(), email, stats])+'\n'
+      );
+    } catch (e) {
+      log("failed to write to build-times.log file: "+e);
+    }
+    done();
+  });
 
   var config = {};
 
