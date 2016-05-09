bf403fc4d8b9ad188c0c75883217c8fa89975e33
Paul Carduner <paul@carduner.net>
2016-05-09 13:27:38 -0700

    Some more display fixes

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -335,7 +335,7 @@ StudioApp.prototype.init = function (config) {
       twitter: config.twitter,
       app: config.app,
       noHowItWorks: config.noHowItWorks,
-      isLegacyShare: config.isLegacyShare
+      isLegacyShare: config.isLegacyShare,
     });
   }
 
@@ -1804,7 +1804,7 @@ StudioApp.prototype.configureDom = function (config) {
   var visualizationColumn = document.getElementById('visualizationColumn');
   var visualization = document.getElementById('visualization');
 
-  if (!config.hideSource || config.embed) {
+  if (!config.hideSource || config.embed || config.level.iframeEmbed) {
     var vizHeight = this.MIN_WORKSPACE_HEIGHT;
     if (this.isUsingBlockly() && config.level.edit_blocks) {
       // Set a class on the main blockly div so CSS can style blocks differently
@@ -1821,7 +1821,7 @@ StudioApp.prototype.configureDom = function (config) {
       config.level.disableVariableEditing = false;
     }
 
-    if (config.iframeEmbed) {
+    if (config.level.iframeEmbed) {
       document.body.className += ' embedded_iframe';
     }
 
@@ -1896,7 +1896,7 @@ StudioApp.prototype.handleHideSource_ = function (options) {
 
         var div = document.createElement('div');
         document.body.appendChild(div);
-        if (!options.iframeEmbed) {
+        if (!options.level.iframeEmbed) {
           ReactDOM.render(React.createElement(WireframeSendToPhone, {
             channelId: dashboard.project.getCurrentId(),
             appType: dashboard.project.getStandaloneApp()
