4ee97c19e09f854e3c5970337dee4bd2c859ceae
Asher Kach <asher@code.org>
2016-05-11 13:43:10 -0500

    merging

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510192122) do
+ActiveRecord::Schema.define(version: 20160511180528) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
