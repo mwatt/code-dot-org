c7c57df96aae56fc0f6f5e7220afac06c7d0a9a5
Asher Kach <asher@code.org>
2016-05-11 13:51:17 -0500

    Update migration timestamps.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511180528) do
+ActiveRecord::Schema.define(version: 20160511210018) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
